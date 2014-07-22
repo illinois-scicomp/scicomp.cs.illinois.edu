13e5deeff7dc0b127339520b95722dbddc6395d9
Andreas Kloeckner <inform@tiker.net>
2014-07-22 17:27:39 -0500

    People tweaks

--- make.sh
+++ make.sh
@@ -3,6 +3,16 @@
 set -e
 set -x
 
+mkdir -p live/images/people/scaled
+for f in ./live/images/people/*jpeg; do
+  dn=$(dirname $f)
+  bn=$(basename $f)
+  scaled="$dn/scaled/$bn"
+  if test "$f" -nt "$scaled"; then
+    convert -geometry 500x500 "$f" "$scaled"
+  fi
+done
+
 rm -f live/*.html
 
 for i in *.html ; do
