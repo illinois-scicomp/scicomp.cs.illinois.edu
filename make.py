691324ddf2223b677c34477eceb75bb48bdf614d
lukeolson <luke.olson@gmail.com>
2017-09-20 09:49:09 -0500

    add courses

--- make.py
+++ make.py
@@ -26,15 +26,39 @@ if os.path.exists(liveweb):
 
 os.makedirs(liveweb)
 
+import re
+import datetime
+urlpattern = re.compile(r'\b(CS|CSE|MATH|ECE)\s*(\d+)')
+year = datetime.datetime.now().year
+
+#newurl = ('<a href="https://courses.illinois.edu/'
+#  'cisapp/dispatcher/catalog/{year}/fall/%s/%s">%s%s</a>'.format(year=year))
+newurl = ('<a href="https://courses.illinois.edu/schedule/terms/%s/%s">%s%s</a>')
+
+def urlify(somestring):
+    """
+    Takes CS357 and turns it into the URL in newurl
+    """
+    return urlpattern.sub(newurl%('\g<1>','\g<2>','\g<1>','\g<2>'), somestring)
+
 with open("people.yml", "r", encoding="utf-8") as inf:
   people = yaml.load(inf)
 
-files = ['_index.html', '_people.html', '_contact.html']
+with open("courses.yml", "r", encoding="utf-8") as inf:
+  courses = yaml.load(inf)
+  courses = [course for course in courses if course['number'] is not None]
+  for course in courses:
+      course['description'] = [urlify(d) for d in course['description'].split('\n\n')]
+
+print(courses[0])
+
+files = ['_index.html', '_people.html', '_contact.html', '_courses.html']
 
 # now render the pages
 for f in files:
     template_vars = {}
     template_vars['people'] = people
+    template_vars['courses'] = courses
 
     template = jinja_env.get_template(f)
     html = template.render(template_vars)
