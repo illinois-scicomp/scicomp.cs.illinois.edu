59ba0ce8785147912ff95f396cdda8d8b002de94
lukeolson <luke.olson@gmail.com>
2017-09-20 20:41:06 -0500

    fixish on the link script

--- checklinks.sh
+++ checklinks.sh
@@ -1,11 +1,11 @@
 #! /bin/bash
 
-# pip install linkchecker
+# pip3 install git+https://github.com/mtlevolio/pylinkchecker.git
 set -e
 set -x
 
-for f in live/*.html ; do
+for f in *.html ; do
   echo "____________________________________"
   echo " checking " $f
-  linkchecker -r1 --check-css --check-html $f
+  pylinkcheck.py -P $f
 done
